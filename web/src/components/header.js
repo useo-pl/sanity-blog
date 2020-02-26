47b7bf54908b0e27fcef9f67eeaac7940f484805
Adrian Pilarczyk <adrian@useo.pl>
2020-02-26 21:58:42 +0100

    Add useo-logo

--- web/src/components/header.js
+++ web/src/components/header.js
@@ -1,30 +1,36 @@
-import {Link} from 'gatsby'
-import React from 'react'
-import Icon from './icon'
-import {cn} from '../lib/helpers'
+import { Link } from "gatsby";
+import React from "react";
+import Icon from "./icon";
+import { cn } from "../lib/helpers";
 
-import styles from './header.module.css'
+import styles from "./header.module.css";
 
-const Header = ({onHideNav, onShowNav, showNav, siteTitle}) => (
+const Header = ({ onHideNav, onShowNav, showNav, siteTitle }) => (
   <div className={styles.root}>
     <div className={styles.wrapper}>
       <div className={styles.branding}>
-        <Link to='/'>{siteTitle}</Link>
+        <Link to="/">
+          <img
+            src="https://www.useo.pl/wp-content/uploads/2018/01/useo_logo_light.png"
+            height="35px"
+            alt="Useo"
+          />
+        </Link>
       </div>
 
       <button className={styles.toggleNavButton} onClick={showNav ? onHideNav : onShowNav}>
-        <Icon symbol='hamburger' />
+        <Icon symbol="hamburger" />
       </button>
 
       <nav className={cn(styles.nav, showNav && styles.showNav)}>
         <ul>
           <li>
-            <Link to='/archive/'>Archive</Link>
+            <Link to="/archive/">Archive</Link>
           </li>
         </ul>
       </nav>
     </div>
   </div>
-)
+);
 
-export default Header
+export default Header;
